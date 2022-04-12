957d6d01f924c8c5520c1d4f4e4ba1aea5d192b7
Corentin Clichy <cor.clichy@gmail.com>
2022-04-12 09:00:00 +0200

    Add API service

--- src/App.jsx
+++ src/App.jsx
@@ -26,62 +26,62 @@ import { reset } from "./features/UserProfile/userProfileSlice";
  * @param {object} props
  * @returns {JSX}
  */
-function App() {
-  const dispatch = useDispatch();
+function App () {
+    const dispatch = useDispatch();
 
-  const [firstTimeLoad, setFirstTimeLoad] = useState(true);
+    const [ firstTimeLoad, setFirstTimeLoad ] = useState(true);
 
-  // Get the location from the react-router-dom
-  const location = useLocation();
-  // Check if location.pathname is equal to "/sign-in"
-  const isBgBlack =
-    location.pathname === "/sign-in" || location.pathname === "/user"
-      ? "true"
-      : "";
+    // Get the location from the react-router-dom
+    const location = useLocation();
+    // Check if location.pathname is equal to "/sign-in"
+    const isBgBlack =
+        location.pathname === "/sign-in"
+            ? "true"
+            : "";
 
-  //Access to local storage and check if remember me is checked
-  const persistLocalStorageState = JSON.parse(
-    localStorage.getItem("persist:UserData")
-  );
+    //Access to local storage and check if remember me is checked
+    const persistLocalStorageState = JSON.parse(
+        localStorage.getItem("persist:UserData")
+    );
 
-  // Get the value of remember me state (true or false)
-  const isRememberMe =
-    persistLocalStorageState &&
-    JSON.parse(persistLocalStorageState.user).data.isRememberMe;
+    // Get the value of remember me state (true or false)
+    const isRememberMe =
+        persistLocalStorageState &&
+        JSON.parse(persistLocalStorageState.user).data.isRememberMe;
 
-  useEffect(() => {
-    console.info("First time load", firstTimeLoad);
+    useEffect(() => {
+        console.info("First time load", firstTimeLoad);
 
-    if (firstTimeLoad) {
-      setFirstTimeLoad(false);
-      if (!isRememberMe) {
-        console.info("Fire clearing localStorage");
-        dispatch(signOut());
-        dispatch(reset());
-      }
-    }
-    return;
-  }, [firstTimeLoad]);
+        if (firstTimeLoad) {
+            setFirstTimeLoad(false);
+            if (!isRememberMe) {
+                console.info("Fire clearing localStorage");
+                dispatch(signOut());
+                dispatch(reset());
+            }
+        }
+        return;
+    }, [ firstTimeLoad ]);
 
-  return (
-    <AppContainer>
-      <Header isLoggedIn />
-      <MainContainer isBgBlack={isBgBlack}>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/sign-in">
-            <SignIn />
-          </Route>
-          <Route path="/user">
-            <User />
-          </Route>
-        </Switch>
-      </MainContainer>
-      <Footer />
-    </AppContainer>
-  );
+    return (
+        <AppContainer>
+            <Header isLoggedIn />
+            <MainContainer isBgBlack={isBgBlack}>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/sign-in">
+                        <SignIn />
+                    </Route>
+                    <Route path="/user">
+                        <User />
+                    </Route>
+                </Switch>
+            </MainContainer>
+            <Footer />
+        </AppContainer>
+    );
 }
 
 // Style
@@ -95,7 +95,7 @@ const AppContainer = styled.div`
 
 const MainContainer = styled.main`
   flex: 1;
-  background-color: ${(props) => (props.isBgBlack ? "#12002b" : "")};
+  background-color: ${(props) => (props.isBgBlack ? "#12002b" : "#dfe6ed")};
 `;
 
 export default App;
--- src/features/Login/userSlice.js
+++ src/features/Login/userSlice.js
@@ -1,66 +1,53 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
 
-import { apiUrl } from "../../app/userAPI";
+import { getToken } from "../../app/userAPI";
+
 
 // Set up the initial state
 const initialState = {
-  data: {
-    token: null,
-    isConnected: null,
-    isRememberMe: false,
-  },
-  status: null,
+    data: {
+        token: null,
+        isConnected: null,
+        isRememberMe: false,
+    },
+    status: null,
 };
 
 export const getUserToken = createAsyncThunk(
-  "user/getUserToken",
-  async ({ email, password, isRememberMe }) => {
-    return await axios
-      .post(`${apiUrl}/login`, {
-        email,
-        password,
-      })
-      .then(function (response) {
+    "user/getUserToken",
+    async ({ email, password, isRememberMe }) => {
+        const res = await getToken(email, password, isRememberMe);
         return {
-          token: response.data.body.token,
-          isConnected: true,
-          isRememberMe: isRememberMe,
+            token: res.data.body.token,
+            isConnected: true,
+            isRememberMe: isRememberMe,
         };
-      })
-      .catch(function (error) {
-        console.log(error);
-        return {
-          data: {},
-          status: "error",
-        };
-      });
-  }
+    }
 );
 
 // Create the slice
 const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    signOut: (state) => {
-      state.data.token = null;
-      state.data.isConnected = false;
-      state.data.isRememberMe = null;
+    name: "user",
+    initialState,
+    reducers: {
+        signOut: (state) => {
+            state.data.token = null;
+            state.data.isConnected = false;
+            state.data.isRememberMe = null;
+        },
     },
-  },
-  extraReducers: {
-    [getUserToken.pending]: (state) => {
-      state.status = "Loading";
+    extraReducers: {
+        [ getUserToken.pending ]: (state) => {
+            state.status = "Loading";
+        },
+        [ getUserToken.fulfilled ]: (state, { payload }) => {
+            state.status = "Succeeded";
+            state.data = payload;
+        },
+        [ getUserToken.rejected ]: (state) => {
+            state.status = "Failed";
+        },
     },
-    [getUserToken.fulfilled]: (state, { payload }) => {
-      state.status = "Succeeded";
-      state.data = payload;
-    },
-    [getUserToken.rejected]: (state) => {
-      state.status = "Failed";
-    },
-  },
 });
 
 export const { signOut } = userSlice.actions;
